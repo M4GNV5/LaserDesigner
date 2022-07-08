bc9c37a863ff38859900c6a93e20116c3016156f
Jakob Löw <jakob@m4gnus.de>
2022-07-08 16:24:37 +0200

    :art: pass the component list to the designer via Vue Props

--- components/LayoutDesigner.vue
+++ components/LayoutDesigner.vue
@@ -1,42 +1,40 @@
 <template>
   <div>
-    <img ref="dummy-logo" style="display: none;" :src="require('~/assets/bn-paf-icon.png')" />
     <canvas ref="draw-area" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
-import { LayoutComponent, MainComponent, TextComponent, ImageComponent } from '../lib/layout-components'
+import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+import { LayoutComponent, MainComponent } from '../lib/layout-components'
 
 @Component
 export default class LayoutDesigner extends Vue {
+  $refs!: {
+    'draw-area': HTMLCanvasElement,
+  }
+
+  root: MainComponent = new MainComponent()
+  ctx: CanvasRenderingContext2D | null = null
+
+  @Prop(Array)
+  readonly components: LayoutComponent[] | undefined
+
+  @Watch('components')
+  onComponentsChanged(val: LayoutComponent[], oldVal: LayoutComponent[]) {
+    console.log('components changed', val, oldVal)
+    this.root.components = val
+    if(this.ctx)
+      this.root.render(this.$refs['draw-area'], this.ctx)
+  }
+
   mounted() {
-    const logo: HTMLImageElement = this.$refs['dummy-logo'] as HTMLImageElement
-    const canvas: HTMLCanvasElement = this.$refs['draw-area'] as HTMLCanvasElement
+    const canvas = this.$refs['draw-area']
     const ctx = canvas.getContext('2d')
+    this.ctx = ctx
 
     if(!ctx)
       return
-
-    const root = new MainComponent()
-
-    const imgComp = new ImageComponent(logo)
-    imgComp.x = 10
-    imgComp.y = 10
-    const text1 = new TextComponent('${excel:column:name}')
-    text1.x = 320
-    text1.y = 10
-    text1.width = 400
-    text1.height = 60
-    const text2 = new TextComponent('${excel:column:description}')
-    text2.x = 10
-    text2.y = 300
-    text2.width = 780
-    text2.height = 40
-
-    root.components.push(imgComp, text1, text2)
 
     let draggingComponent: LayoutComponent | null = null
     let resizingComponent: LayoutComponent | null = null
@@ -45,21 +43,20 @@ export default class LayoutDesigner extends Vue {
     let originalWidth = 0
     let originalHeight = 0
 
-
-    root.render(canvas, ctx)
+    this.root.render(canvas, ctx)
 
     canvas.onmousedown = ev => {
-      for(let comp of root.components) {
+      for(let comp of this.root.components) {
         if(ev.offsetX >= comp.x + comp.width - 3 && ev.offsetX <= comp.x + comp.width + 3
           && ev.offsetY >= comp.y + comp.height - 3 && ev.offsetY <= comp.y + comp.height + 3) {
-          root.activeComponent = comp
+          this.root.activeComponent = comp
           resizingComponent = comp
           originalWidth = comp.width
           originalHeight = comp.height
         }
         else if(ev.offsetX >= comp.x && ev.offsetX <= comp.x + comp.width - 3
           && ev.offsetY >= comp.y && ev.offsetY <= comp.y + comp.height - 3) {
-          root.activeComponent = comp
+          this.root.activeComponent = comp
           draggingComponent = comp
           mouseStartX = ev.offsetX - comp.x
           mouseStartY = ev.offsetY - comp.y
@@ -71,7 +68,7 @@ export default class LayoutDesigner extends Vue {
       if(draggingComponent) {
         draggingComponent.x = ev.offsetX - mouseStartX
         draggingComponent.y = ev.offsetY - mouseStartY
-        root.render(canvas, ctx)
+        this.root.render(canvas, ctx)
       }
       if(resizingComponent) {
         const scaleW = Math.max(0, ev.offsetX - resizingComponent.x) / originalWidth
@@ -80,7 +77,7 @@ export default class LayoutDesigner extends Vue {
 
         resizingComponent.width = Math.floor(originalWidth * scale)
         resizingComponent.height = Math.floor(originalHeight * scale)
-        root.render(canvas, ctx)
+        this.root.render(canvas, ctx)
       }
     }
 
---
+++ pages/edit.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              Projekte
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+    <div class="main">
+      <img ref="dummy-logo" style="display: none;" :src="require('~/assets/bn-paf-icon.png')" />
+      <LayoutDesigner :components="components" />
+    </div>
+  </div>
+
+
+</template>
+
+<style scpoped>
+  .main {
+    padding: 50px;
+    width: 70%;
+  }
+
+  .top {
+
+    padding-top: 10px;
+  }
+
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { TextComponent, ImageComponent, LayoutComponent } from '../lib/layout-components'
+
+@Component
+export default class EditPage extends Vue {
+  $refs!: {
+    'dummy-logo': HTMLImageElement,
+  }
+
+  components: LayoutComponent[] = []
+
+  mounted() {
+    const logo = this.$refs['dummy-logo']
+
+    const imgComp = new ImageComponent(logo)
+    imgComp.x = 10
+    imgComp.y = 10
+
+    const text1 = new TextComponent('${excel:column:name}')
+    text1.x = 320
+    text1.y = 10
+    text1.width = 400
+    text1.height = 60
+
+    const text2 = new TextComponent('${excel:column:description}')
+    text2.x = 10
+    text2.y = 300
+    text2.width = 780
+    text2.height = 40
+
+    this.components = [imgComp, text1, text2]
+    console.log('created components')
+  }
+}
+</script>
